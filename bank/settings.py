fffaab43eefa17c3e64a85286e3d2b29c557bb72
oluwasegun5 <adeayosegun5@gmail.com>
2022-09-14 09:40:51 +0100

    hide secret key

--- bank/settings.py
+++ bank/settings.py
@@ -14,13 +14,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(BASE_DIR/".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4(8liur=jn6tis6mcj3)obc_!oqq-^o$-b0ti+%lufew#%w8p6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
