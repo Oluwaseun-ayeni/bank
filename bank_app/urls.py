ab1c51d308cff28c4c1fbafd5c62f6ec0f784718
oluwasegun5 <adeayosegun5@gmail.com>
2022-09-16 12:05:06 +0100

    added id to customer url path

--- bank_app/urls.py
+++ bank_app/urls.py
@@ -2,14 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('customer/', views.customer),
+    path('customer/<int:pk>/', views.customer),
     path('create/', views.create_customer),
     path('update/<int:id>', views.update_customer),
     path('delete/<int:id>', views.delete_customer)
-]
-
-
-urlpatterns = [
-    path('register/',views.register),
-    path('login/',views.login),
 ]
